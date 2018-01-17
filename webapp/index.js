2e6d00b7d7794af28c60d570ca628daebe1d0e61
Gibran Lopez Morales <gibran@michelada.io>
2018-01-17 16:02:34 -0600

    implements HMR for JS files

--- webapp/index.js
+++ webapp/index.js
@@ -4,14 +4,6 @@ import ReactDOM from 'react-dom';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// let root = document.getElementById('root');
-// let p = document.createElement('p');
-// let printText = () => {
-//   p.textContent = App('Gibran 3');
-// };
-// printText();
-// root.appendChild(p);
-
 if (module.hot) {
   module.hot.accept();
 }
